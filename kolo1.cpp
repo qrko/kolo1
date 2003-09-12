ddb770cab417f40ff0c2bbf00369cdb4b98fcce5
Arek Machnik <qrkopl@gmail.com>
2003-09-12 06:30:27 +0200

    odpowiedz na pytanie

--- kolo1.cpp
+++ kolo1.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 using namespace std;
 
+void pytanie()
+{
+  cout<<"Q1) Jak wyswietlic roznice miedzy drugim a trzecim zatwierdzeniem wykonanym w galezi master?"<<endl;
+  cout<<"A1) git diff HEAD^^ HEAD^^^"<<endl;
+  cout<<"Q2) Co sie zmienilo?"<<endl;
+  cout<<"A2) zmienil sie punkt pierwszy (rperlinski/kolo1 na kolo1/kolo1)"<<endl;
+}
+
 int main(int argc, char **argv) {
   cout<<"I don't like git"<<endl;
   return 0;
